07aed68d987a10298aa4e1b86f66e72777957a27
purch <ucheprince26@gmail.com>
2025-05-22 09:15:35 +0100

    feat: add Favorites component to display user's favorite movies

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,15 @@
+import { useMoveieContext } from "../contexts/MovieContext";
+import "../css/Favorites.css";
+
+function Favorites() {
+  const { favorites } = useMoveieContext();
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorites Movies Yet</h2>
+      <p>Start adding movies to your favorite</p>
+    </div>
+  );
+}
+
+export default Favorites;
