475893dd42baeeacbf7a801c2d5f8959b90a462f
purch <ucheprince26@gmail.com>
2025-05-22 10:09:21 +0100

    feat: implement favorites display in Favorites component

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,23 @@
+import MovieCard from "../components/MovieCard";
 import { useMoveieContext } from "../contexts/MovieContext";
 import "../css/Favorites.css";
 
 function Favorites() {
   const { favorites } = useMoveieContext();
+
+    if (favorites) {
+      return (
+        <div className="favorites">
+          <h2>Your Favorite Movies</h2>
+          <p>{favorites.length} movies</p>
+            <div className="movies-grid">
+            {favorites.map((movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+            ))}
+            </div>
+        </div>
+      );
+    }
 
   return (
     <div className="favorites-empty">
