5ee186ee125dcb1d79d4b537982f8c0f0e2fc609
purch <ucheprince26@gmail.com>
2025-05-22 17:59:03 +0100

    fix: prevent event propagation on favorite button click in MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { useMoveieContext } from "../contexts/MovieContext";
+import { Link } from "react-router-dom";
 import "../css/MovieCard.css";
 
 function MovieCard({movie}) {
@@ -7,6 +8,7 @@ function MovieCard({movie}) {
 
     function onFavoriteClick(e) {
         e.preventDefault();
+        e.stopPropagation();
         if (favorite) {
             removeFromFavorites(movie.id);
         } else {
@@ -14,20 +16,22 @@ function MovieCard({movie}) {
         }
     }
   return (
-    <div className="movie-card">
-        <div className="movie-poster">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-         <div className="movie-overlay">
-            <button className={`favorite-btn ${favorite ? 'active' : ''}`} onClick={onFavoriteClick}>{favorite ? '❤️' : '🤍'}</button>
-            
-         </div>
-        </div>
+    <Link to={`/movie/${movie.id}`} className="movie-card-link">
+      <div className="movie-card">
+          <div className="movie-poster">
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+           <div className="movie-overlay">
+              <button className={`favorite-btn ${favorite ? 'active' : ''}`} onClick={onFavoriteClick}>{favorite ? '❤️' : '🤍'}</button>
+              <div className="view-details">View Details</div>
+           </div>
+          </div>
 
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date?.split('-')[0]}</p>
-        </div>
-    </div>
+          <div className="movie-info">
+              <h3>{movie.title}</h3>
+              <p>{movie.release_date?.split('-')[0]}</p>
+          </div>
+      </div>
+    </Link>
   );
 }
 
